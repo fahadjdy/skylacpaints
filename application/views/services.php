7c7740cf12f7c4a0def186500b339c012f4b95c7
fahadjdy <fahadjdy12@gmail.com>
2024-12-24 22:03:06 +0530

    code updated

--- application/views/services.php
+++ application/views/services.php
@@ -41,32 +41,7 @@
             <div class="about-area5 about-p p-relative">
                 <div class="container pt-120 pb-90">
                     <div class="row">
-                         <!-- #right side -->
-                        <!-- <div class="col-sm-12 col-md-12 col-lg-4 order-1">
-                           <aside class="sidebar services-sidebar"> -->
-                        
-                        <!-- Category Widget -->
-                        <!-- <div class="sidebar-widget categories">
-                            <div class="widget-content">
-                                <h2 class="widget-title"> Services List </h2> -->
-                                <!-- Services Category -->
-                                <!-- <ul class="services-categories"> -->
-                                    <?php // foreach($category as $categories): ?>
-                                    <!-- <li <?php// if($data->c_url == $categories->c_url) echo 'class="active"'; ?>> -->
-                                        <!-- <a href="<?= base_url('services/'.$categories->c_url)?>"><?=$categories->c_name?></a> -->
-                                    <!-- </li> -->
-                                    <?php // endforeach; ?>
-                                <!-- </ul>
-                            </div>
-                        </div> -->
-                        <!--Service Contact-->
-                        <!-- <div class="service-detail-contact wow fadeup-animation" data-wow-delay="1.1s">
-                            <h3 class="h3-title">If You Need Any Help Contact With Us</h3>
-                            <a href="javascript:void(0);" title="Call now"><?=$profile[0]->phone?></a>
-                        </div>
-                    </aside>
-                        </div> -->
-                        <!-- #right side end -->
+                       
                        
                         
 					<div class="col-lg-12 col-md-12 col-sm-12 order-2">
